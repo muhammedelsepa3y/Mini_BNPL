1583da931e6e77cfefcb9477372aaa9c9051714c
Mohamed Elsebaey <muhammedelsepa3y@gmail.com>
2026-03-13 20:27:54 +0200

    fix: Remove order details route from app router

--- lib/core/routes/app_route_const.dart
+++ lib/core/routes/app_route_const.dart
@@ -4,13 +4,11 @@ class AppRouteConst {
   static const String products = '/products';
   static const String productDetails = '/products/details/:id';
   static const String orders = '/orders';
-  static const String orderDetails = '/orders/details/:id';
 
   // Names
   static const String splashName = 'splash';
   static const String productsName = 'products';
   static const String productDetailsName = 'product_details';
   static const String ordersName = 'orders';
-  static const String orderDetailsName = 'order_details';
 
 }
